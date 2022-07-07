00d18ad41c0dad7b6afec52b659e3f65cac77958
Nonso30 <preciousenoch29@gmail.com>
2022-07-07 04:11:07 -0700

    0. _putchar task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Print "_putchar"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	char name[] = "_putchar";
+
+	while (i < 8)
+	{
+		_putchar(name[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
